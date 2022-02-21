c340352966a0fc023e0c41644aa034e66cb1854e
igabriel39 <gabriel.mendonca2@hotmail.com>
2022-02-21 00:15:02 -0300

    Questão 3

--- src/DesafioProgramacao/Quest3.java
+++ src/DesafioProgramacao/Quest3.java
@@ -1,27 +1,35 @@
 package DesafioProgramacao;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Quest3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Digite a string desejada");
 
-        System.out.println("Digite a string: ");
         String s = input.next();
-        int len = 1;
+        int qtd = 1;
         int gap = 1;
-        while(gap <= s.length()){
+
+        while(gap < s.length()){
             String subs = s.substring(0,gap);
-            System.out.println(subs);
             int i = 1;
             int j = i+gap;
             int x = i;
             while(j <= s.length()){
-                System.out.println(subs.equals(s.substring(x,j)));
+                char[] subsArr = subs.toCharArray();
+                char[] subsArrComparacao = s.substring(x,j).toCharArray();
+                Arrays.sort(subsArr);
+                Arrays.sort(subsArrComparacao);
+                if(Arrays.toString(subsArr).equals(Arrays.toString(subsArrComparacao))){
+                    qtd++;
+                }
                 x++;
                 j++;
             }
             gap++;
         }
+        System.out.println(qtd);
     }
 }
